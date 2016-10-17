f7306ea9714c3b09b1fb11e1af50eac39c5adcf3
Zia Siddique <zia.siddique@gibedigital.com>
2016-10-17 15:53:05 +0100

    Implement GibeCommerce navigation provider

---
+++ Gibe.Navigation.GibeCommerce/GibeCommerceNavigationProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gibe.Navigation.GibeCommerce.Models;
+using Gibe.Navigation.Models;
+using Gibe.Urls;
+using GibeCommerce.CatalogSystem;
+using GibeCommerce.CatalogSystem.Data;
+using GibeCommerce.SiteServices.UrlProviders;
+
+namespace Gibe.Navigation.GibeCommerce
+{
+    public class GibeCommerceNavigationProvider<T> : INavigationProvider<T> where T : INavigationElement
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IUrlProvider _urlProvider;
+        // URL provider
+
+        public GibeCommerceNavigationProvider(ICatalogService catalogService, int priority, IUrlProvider urlProvider)
+        {
+            _catalogService = catalogService;
+            Priority = priority;
+            _urlProvider = urlProvider;
+        }
+
+        public IEnumerable<T> GetNavigationElements()
+        {
+            var categories = _catalogService.GetSubCategories("root")
+                .Where(x => x.Name != "root" && IncludeInNavigation(x));
+            
+            return categories.OrderBy(x => x.Rank).Select(x => (T)ToNavigationElement(x)).ToList();
+        }
+
+        public SubNavigationModel<T> GetSubNavigation(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Priority { get; }
+
+        private INavigationElement ToNavigationElement(Category category)
+        {
+            return new GibeCommerceNavigationElement
+            {
+                Title = category.PageTitle,
+                IsActive = false,
+                IsVisible = ShowInNavigation(category),
+                NavTitle = category.DisplayName,
+                Items = _catalogService.GetSubCategories(category.Name).Select(ToNavigationElement),
+                Url = _urlProvider.GetUrl(category, UrlProviderMode.Relative)
+            };
+        }
+
+        protected virtual bool IncludeInNavigation(Category category)
+        {
+            return true;
+        }
+
+        protected virtual bool ShowInNavigation(Category category)
+        {
+            return true;
+        }
+    }
+}
--- Gibe.Navigation.Umbraco/Models/UmbracoNavigationElement.cs
+++ Gibe.Navigation.Umbraco/Models/UmbracoNavigationElement.cs
@@ -1,35 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Gibe.Navigation.Models;
 using Our.Umbraco.Ditto;
 
 namespace Gibe.Navigation.Umbraco.Models
 {
-	public class UmbracoNavigationElement : INavigationElement
-	{
-		[UmbracoProperty("Name")]
-		public string Title { get; set; }
-		public string NavTitle { get; set; }
-		public string Url { get; set; }
-		public bool IsActive { get; set; }
-		public IEnumerable<INavigationElement> Items { get; set; }
-		public string Target => "_self";
-		public bool IsVisible { get; set; }
-		public bool IsConcrete => true;
-		public object Clone()
-		{
-			return new UmbracoNavigationElement
-			{
-				Title = Title,
-				NavTitle = NavTitle,
-				Url = Url,
-				IsActive = IsActive,
-				Items = Items.Select(i => (INavigationElement) i.Clone()).ToList(),
-				IsVisible = IsVisible,
-			};
-		}
-	}
+    public class UmbracoNavigationElement : INavigationElement
+    {
+        [UmbracoProperty("Name")]
+        public string Title { get; set; }
+
+        public string NavTitle { get; set; }
+        public string Url { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<INavigationElement> Items { get; set; }
+        public string Target => "_self";
+        public bool IsVisible { get; set; }
+        public bool IsConcrete => true;
+
+        public object Clone()
+        {
+            return new UmbracoNavigationElement
+            {
+                Title = Title,
+                NavTitle = NavTitle,
+                Url = Url,
+                IsActive = IsActive,
+                Items = Items.Select(i => (INavigationElement) i.Clone()).ToList(),
+                IsVisible = IsVisible
+            };
+        }
+    }
 }
