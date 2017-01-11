f8bb064785f56fbd3be2e5ed109d1cf9552608e8
Zia Siddique <zia.siddique@gibedigital.com>
2017-01-11 10:15:44 +0000

    Implement NavigationAttribute and FakeRootNodeType

---
+++ Gibe.Navigation/NavigationAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Gibe.DittoProcessors.Processors;
+using Gibe.Navigation.Models;
+
+namespace Gibe.Navigation
+{
+	public class NavigationAttribute : TestableDittoProcessorAttribute
+	{
+		private readonly INavigationService<INavigationElement> _navigationService;
+
+		public NavigationAttribute(INavigationService<INavigationElement> navigationService)
+		{
+			_navigationService = navigationService;
+		}
+
+		public NavigationAttribute()
+		{
+			_navigationService = DependencyResolver.Current.GetService<INavigationService<INavigationElement>>();
+		}
+
+		public override object ProcessValue()
+			=> _navigationService.GetNavigation(Context.Content.Url);
+	}
+}
