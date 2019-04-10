564becee6d39da3d19dba51a5de2332e28c4f60c
Steve Temple <steve@gibedigital.com>
2019-04-10 15:32:50 +0100

    Add composing

---
+++ Gibe.Navigation.Umbraco/GibeNavigationComposer.cs
@@ -0,0 +1,18 @@
+﻿using Gibe.Navigation.Models;
+using Gibe.Navigation.Umbraco.Filters;
+using Umbraco.Core.Composing;
+
+namespace Gibe.Navigation.Umbraco
+{
+	public class GibeNavigationComposer : IComposer
+	{
+		public void Compose(Composition composition)
+		{
+			composition.RegisterFor<INodeTypeFactory, DefaultNodeTypeFactory>();
+			composition.RegisterFor<INavigationElementFactory, NavigationElementFactory>();
+			composition.RegisterFor<INavigationProvider<INavigationElement>, UmbracoNavigationProvider<INavigationElement>>();
+			composition.RegisterFor<IUmbracoNodeService, UmbracoNodeService>();
+			composition.RegisterFor<INavigationFilter, TemplateOrRedirectFilter>();
+		}
+	}
+}
