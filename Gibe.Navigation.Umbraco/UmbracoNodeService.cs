9ebe83e658b56753ff223c5fbf08ad869ce95008
Steve Temple <steve@gibedigital.com>
2021-07-08 12:50:33 +0100

    Fix for out of process node finding

--- Gibe.Navigation.Umbraco/UmbracoNodeService.cs
+++ Gibe.Navigation.Umbraco/UmbracoNodeService.cs
@@ -1,59 +1,26 @@
-﻿using System.Collections.Generic;
-using Gibe.Navigation.Umbraco.NodeTypes;
+﻿using Gibe.Navigation.Umbraco.NodeTypes;
 using JetBrains.Annotations;
-using Moq;
-using NUnit.Framework;
 using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Web.PublishedCache;
+using Umbraco.Web;
 
 namespace Gibe.Navigation.Umbraco
 {
 	public class UmbracoNodeService : IUmbracoNodeService
 	{
-		private readonly IPublishedSnapshotService _publishedContentCache;
+		private readonly IUmbracoContextFactory _umbracoContextFactory;
 		
-		public UmbracoNodeService(IPublishedSnapshotService publishedContentCache)
+		public UmbracoNodeService(IUmbracoContextFactory umbracoContextFactory)
 		{
-			_publishedContentCache = publishedContentCache;
+			_umbracoContextFactory = umbracoContextFactory;
 		}
 
 		[NotNull]
 		public IPublishedContent GetNode([NotNull]INodeType nodeType)
 		{
-			return nodeType.FindNode(_publishedContentCache.PublishedSnapshotAccessor.PublishedSnapshot.Content.GetAtRoot());
+			using (var context = _umbracoContextFactory.EnsureUmbracoContext())
+			{
+				return nodeType.FindNode(context.UmbracoContext.Content.GetAtRoot());
+			}
 		}
 	}
-	
-	[TestFixture]
-	internal class UmbracoNodeServiceTests
-	{
-		[Test]
-		public void GetNode_Calls_FindNode_On_NodeType()
-		{
-			var mockContent = new Mock<IPublishedContent>().Object;
-
-			var publishedContentCache = new Mock<IPublishedContentCache>();
-			publishedContentCache.Setup(p => p.GetAtRoot(null))
-				.Returns(new List<IPublishedContent> { mockContent});
-
-			var publishedSnapshot = new Mock<IPublishedSnapshot>();
-			publishedSnapshot.Setup(p => p.Content)
-				.Returns(publishedContentCache.Object);
-
-			var publishedContentAccessor = new Mock<IPublishedSnapshotAccessor>();
-			publishedContentAccessor.Setup(p => p.PublishedSnapshot)
-				.Returns(publishedSnapshot.Object);
-
-			var publishedSnapshotService = new Mock<IPublishedSnapshotService>();
-			publishedSnapshotService.Setup(u => u.PublishedSnapshotAccessor)
-				.Returns(publishedContentAccessor.Object);
-			
-			var nodeService = new UmbracoNodeService(publishedSnapshotService.Object);
-			var result = nodeService.GetNode(new FakeNodeType(mockContent));
-
-			Assert.That(result, Is.EqualTo(mockContent));
-		}
-	}
-	
-
 }
