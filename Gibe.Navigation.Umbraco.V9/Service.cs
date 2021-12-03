54b275fd09fcd4d5c6b4313d53ffd3bdcb84f577
Steve Temple <steve@gibedigital.com>
2021-12-03 21:31:43 +0000

    [v9] Add back v9 service.cs

---
+++ Gibe.Navigation.Umbraco.V9/Service.cs
@@ -0,0 +1,22 @@
+﻿using Gibe.Navigation.Umbraco.Filters;
+using Gibe.Navigation.Umbraco.NodeTypes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gibe.Navigation.Umbraco
+{
+	public static class Services
+	{
+		public static void AddNavigation(this IServiceCollection services)
+		{
+			services.AddTransient<INavigationService, DefaultNavigationService>();
+			services.AddTransient<INodeTypeFactory, DefaultNodeTypeFactory>();
+			services.AddTransient<INavigationProvider, UmbracoNavigationProvider>();
+			services.AddTransient<IUmbracoNodeService, UmbracoNodeService>();
+			services.AddTransient<INavigationFilter, TemplateOrRedirectFilter>();
+			services.AddTransient<INavigationElementFactory, NavigationElementFactory>();
+
+			services.AddTransient<INodeType, SettingsNodeType>();
+			services.AddTransient<ICache, MemoryCacheWrapper>();
+		}
+	}
+}
